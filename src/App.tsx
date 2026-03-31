f8d663abe08bff3c2c3273eb156db00ebb2222b7
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-03-31 22:49:03 +0300

    feat: FSD structure folder and routing setup

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      <section>empty project</section>
-    </>
-  );
-}
-
-export default App;
---
+++ src/app/providers/router/routerConfig.tsx
@@ -0,0 +1,11 @@
+import type { RouteProps } from "react-router";
+
+import { HomePage } from "@/pages/Home";
+import { AppRoutes, routePaths } from "@/shared/config";
+
+export const routeConfig: RouteProps[] = [
+  {
+    path: routePaths[AppRoutes.HOME],
+    element: <HomePage />,
+  },
+];
