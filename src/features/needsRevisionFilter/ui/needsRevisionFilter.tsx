3be0d9ad30a84641473a7bc35358adfa0bb3e882
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-04-02 17:36:28 +0300

    feat: reset filter login and react Tumbler on reset

--- src/features/needsRevisionFilter/ui/needsRevisionFilter.tsx
+++ src/features/needsRevisionFilter/ui/needsRevisionFilter.tsx
@@ -2,31 +2,28 @@ import { Tumbler } from "@/shared/ui";
 import "./needsRevisionFilter.style.css";
 import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import { selectSearchParams, setSearchParams } from "@/entities/ad/model";
-import { useState } from "react";
 import { getAds } from "@/entities/ad/api";
 
 export const NeedsRevisionFilter = () => {
   const dispatch = useAppDispatch();
   const searchParams = useAppSelector(selectSearchParams);
-  const [isChecked, setIsChecked] = useState(
-    searchParams.needsRevision === true,
-  );
+  const isChecked = searchParams.needsRevision === true;
 
   const handleToggle = (checked: boolean) => {
-    setIsChecked(checked);
-
-    const newParams = {
-      ...searchParams,
-      needsRevision: checked ? true : undefined,
-    };
+    const newNeedsRevision = checked ? true : undefined;
 
     dispatch(
       setSearchParams({
-        needsRevision: checked ? true : undefined,
+        needsRevision: newNeedsRevision,
       }),
     );
 
-    dispatch(getAds(newParams));
+    dispatch(
+      getAds({
+        ...searchParams,
+        needsRevision: newNeedsRevision,
+      }),
+    );
   };
   return (
     <div className="filter-revision">
--- src/features/resetFilter/ui/resetFilter.tsx
+++ src/features/resetFilter/ui/resetFilter.tsx
@@ -1,5 +1,29 @@
+import { useAppDispatch, useAppSelector } from "@/app/store/hooks";
 import "./resetFilter.style.css";
+import { selectSearchParams, setSearchParams } from "@/entities/ad/model";
+import { getAds } from "@/entities/ad/api";
 
 export const ResetFilter = () => {
-  return <button className="filter-reset">Сбросить фильтр</button>;
+  const dispatch = useAppDispatch();
+  const searchParams = useAppSelector(selectSearchParams);
+
+  const handleResetFilters = () => {
+    const resetParams = {
+      ...searchParams,
+      categories: undefined,
+      needsRevision: undefined,
+      skip: 0,
+    };
+
+    dispatch(
+      setSearchParams({ categories: undefined, needsRevision: undefined }),
+    );
+    dispatch(getAds(resetParams));
+  };
+
+  return (
+    <button className="filter-reset" onClick={handleResetFilters}>
+      Сбросить фильтр
+    </button>
+  );
 };
