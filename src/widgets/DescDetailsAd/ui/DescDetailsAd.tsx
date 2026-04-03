6709156083c873d6047c8e0f88623f98a5c74c3b
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-04-03 23:50:12 +0300

    feat: display info in desc

--- src/widgets/DescDetailsAd/ui/DescDetailsAd.tsx
+++ src/widgets/DescDetailsAd/ui/DescDetailsAd.tsx
@@ -1,14 +1,22 @@
+import { useAppSelector } from "@/app/store/hooks";
 import "./DescDetailsAd.style.css";
+import { selectCurrentAd } from "@/entities/ad/model";
 
 export const DescDetailsAd = () => {
+  const ad = useAppSelector(selectCurrentAd);
+
   return (
     <div className="ads-details__desc">
       <p className="ads-details__desc-title">Описание</p>
-      <h2 className="ads-details__desc-text">
-        Продаю свой MacBook Pro 16" (2021) на чипе M1 Pro. Состояние отличное,
-        работал бережно. Мощности хватает на всё: от сложного монтажа до кода,
-        при этом ноутбук почти не греется.
-      </h2>
+      {ad?.description ? (
+        <h2 className="ads-details__desc-text">
+          Продаю свой MacBook Pro 16" (2021) на чипе M1 Pro. Состояние отличное,
+          работал бережно. Мощности хватает на всё: от сложного монтажа до кода,
+          при этом ноутбук почти не греется.
+        </h2>
+      ) : (
+        <p className="ads-details__desc-text">Отсутсвует</p>
+      )}
     </div>
   );
 };
