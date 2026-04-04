6fc49f6a7edb671230577bce966bf080f97d7b6a
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-04-05 00:01:32 +0300

    feat: edit settings for cors

--- src/shared/api/axios/httpClient.ts
+++ src/shared/api/axios/httpClient.ts
@@ -1,8 +1,7 @@
-import { API_URL } from "@/shared/config";
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: API_URL,
+  baseURL: "/api",
   withCredentials: false,
   timeout: 50000,
 });
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    open: true,
-    host: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      open: true,
+      host: true,
+      proxy: {
+        "/api": {
+          target: env.VITE_API_PROXY_TARGET || "http://localhost:8080",
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
+      },
     },
-  },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  };
 });
