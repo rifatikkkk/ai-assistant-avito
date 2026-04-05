560da82cf987a800eb4e876e12342b66a45675c4
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-04-05 19:33:41 +0300

    feat: add link to ads page from home

--- src/pages/Home/ui/HomePage.tsx
+++ src/pages/Home/ui/HomePage.tsx
@@ -1,3 +1,18 @@
+import { useNavigate } from "react-router";
+import "./HomePage.style.css";
+import { Button } from "@/shared/ui";
+
 export const HomePage = () => {
-  return <div>Empty home page</div>;
+  const navigate = useNavigate();
+
+  const handleToAds = () => {
+    navigate("/ads");
+  };
+
+  return (
+    <div className="home-page">
+      Empty home page
+      <Button onClick={handleToAds}>К объявлениям</Button>
+    </div>
+  );
 };
