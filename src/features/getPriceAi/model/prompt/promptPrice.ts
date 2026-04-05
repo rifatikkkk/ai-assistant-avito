a92debeec93db808c770b1672e0221203555e92a
rifatikkkk <149669085+rifatikkkk@users.noreply.github.com>
2026-04-05 18:14:11 +0300

    fix: delete comment in pricePrompt

--- src/features/getPriceAi/model/prompt/promptPrice.ts
+++ src/features/getPriceAi/model/prompt/promptPrice.ts
@@ -1,44 +1,15 @@
 import type { FormDataUpdate } from "@/entities/ad/model";
 
 export const promptPrice = (item: FormDataUpdate) => {
-  const { title, price, description, category, params } = item;
+  const { title, price } = item;
 
-  let prompt =
+  const prompt =
     `Напиши мне примерную стоимость ${title || "товара"} и диапазон цен (числа пиши без точек и пробелов).` +
+    `На данный момент я оцениваю товара в ${price || 0} рублей.` +
     `Будь краток и конкретен. При ответе не пиши лишние комментарии, напиши строго по формату (заголовок и 2 пункта):` +
     `Средняя цена на ${title}\n` +
     `1. Примерная цена: [число] ₽` +
     `2. Диапазон цен: [число] ₽`;
-
-  // for more details
-  // if (category === "auto") {
-  //   prompt +=
-  //     `Категория: Автомобиль` +
-  //     `Характеристики:` +
-  //     `${params.brand ? `- Бренд: ${params.brand}` : ""}` +
-  //     `${params.model ? `- Модель: ${params.model}` : ""}` +
-  //     `${params.yearOfManufacture ? `- Год выпуска: ${params.yearOfManufacture}` : ""}` +
-  //     `${params.transmission ? `- Коробка передач: ${params.transmission === "automatic" ? "Автомат" : "Механика"}` : ""}` +
-  //     `${params.mileage ? `- Пробег: ${params.mileage} км` : ""}` +
-  //     `${params.enginePower ? `- Мощность: ${params.enginePower} л.с.` : ""}`;
-  // } else if (category === "real_estate") {
-  //   prompt +=
-  //     `Категория: Недвижимость` +
-  //     `Характеристики:` +
-  //     `${params.type ? `- Тип: ${params.type === "flat" ? "Квартира" : params.type === "house" ? "Дом" : "Комната"}` : ""}` +
-  //     `${params.address ? `- Адрес: ${params.address}` : ""}` +
-  //     `${params.area ? `- Площадь: ${params.area} м²` : ""}` +
-  //     `${params.floor ? `- Этаж: ${params.floor}` : ""}`;
-  // } else if (category === "electronics") {
-  //   prompt +=
-  //     `Категория: Электроника` +
-  //     `Характеристики:` +
-  //     `${params.type ? `- Тип: ${params.type === "phone" ? "Телефон" : params.type === "laptop" ? "Ноутбук" : "Разное"}` : ""}` +
-  //     `${params.brand ? `- Бренд: ${params.brand}` : ""}` +
-  //     `${params.model ? `- Модель: ${params.model}` : ""}` +
-  //     `${params.condition ? `- Состояние: ${params.condition === "new" ? "Новое" : "Б/у"}` : ""}` +
-  //     `${params.color ? `- Цвет: ${params.color}` : ""}`;
-  // }
 
   return prompt;
 };
